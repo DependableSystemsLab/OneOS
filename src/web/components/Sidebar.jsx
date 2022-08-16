cb056ebdf0f035b184cfe7c2522557ebd6edc745
ashikka <ashikagupta28@gmail.com>
2022-08-16 13:34:28 -0700

    feat: Save file to fs

--- src/web/components/Sidebar.jsx
+++ src/web/components/Sidebar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import OneOSService from '../services/OneOSService.jsx';
+import Swal from 'sweetalert2'
 
-
-export default ({nodes, edges}) => {
+const Sidebar = ({ nodes, edges, sys }) => {
   const onDragStart = (event, nodeType) => {
     event.dataTransfer.setData('application/reactflow', nodeType);
     event.dataTransfer.effectAllowed = 'move';
   };
+
+  function saveFile() {
+    Swal.fire(
+      'Filed saved!',
+      'Please go to the file system tab to view the file.',
+      'success'
+    )
+    sys.writeFileSystem("/", {
+      type: 'file',
+      name: 'test.js',
+      content: JSON.stringify({nodes,edges})
+    }).then((result) => {
+      console.log(result);
+    })
+  }
 
   return (
     <div className="sidebar">
@@ -20,7 +36,14 @@ export default ({nodes, edges}) => {
       <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
         Output Node
       </div>
-      <Button color='green' onClick={console.log(nodes, edges)}>Save</Button>
+      <Button color='green' onClick={() => saveFile()}>Save</Button>
     </div>
   );
 };
+export default (props) => (
+  <OneOSService.Consumer>
+    {
+      (sys) => (<Sidebar {...props} sys={sys} />)
+    }
+  </OneOSService.Consumer>
+)
