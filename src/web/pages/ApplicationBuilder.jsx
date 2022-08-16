b464ee3f52201399a97bd66cacf1b9feadf5045f
ashikka <ashikagupta28@gmail.com>
2022-08-16 15:30:10 -0700

    fix: Empty input for graph name

--- src/web/pages/ApplicationBuilder.jsx
+++ src/web/pages/ApplicationBuilder.jsx
@@ -27,7 +27,7 @@ const initialNodes = [
 let id = 0;
 const getId = () => `dndnode_${id++}`;
 
-const ApplicationBuilder = ({sys}) => {
+const ApplicationBuilder = ({ sys }) => {
     const reactFlowWrapper = useRef(null);
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState([]);
@@ -39,162 +39,172 @@ const ApplicationBuilder = ({sys}) => {
     const [selectedNode, setSelectNode] = useState(null);
 
     function saveFile() {
-        Swal.fire(
-            'Filed saved!',
-            `Please go to the file system tab to view the file: ${graphName}`,
-            'success'
-        )
-        sys.writeFileSystem("/", {
-            type: 'file',
-            name: graphName,
-            content: JSON.stringify({ nodes, edges }, null, "\t")
-        }).then((result) => {
-            console.log(result);
-        })
-    }
 
-    function onNodesSelected(node) {
-        if (node) {
-            setNodeName(node.data.label);
-            setNodeBg(node.style.backgroundColor);
-            setSelectNode(node);
+        if (graphName === '') {
+
+            Swal.fire(
+                'Error',
+                'Please enter a name for the graph',
+                'error',
+            )
+            return;
         }
-    }
-
-    function changeNodeName(name) {
-        if (selectedNode) {
-            setNodeName(name);
-        }
-    }
-
-    function changeBgColor(color) {
-        if (selectedNode) {
-            setNodeBg(color);
-        }
-    }
-
-    useEffect(() => {
-        setNodes((nds) =>
-            nds.map((node) => {
-                if (selectedNode != null && node.id === selectedNode.id) {
-                    node.data = {
-                        ...node.data,
-                        label: nodeName,
-                    };
-                }
-
-                return node;
+            Swal.fire(
+                'Filed saved!',
+                `Please go to the file system tab to view the file: ${graphName}`,
+                'success'
+            )
+            sys.writeFileSystem("/", {
+                type: 'file',
+                name: graphName,
+                content: JSON.stringify({ nodes, edges }, null, "\t")
+            }).then((result) => {
+                console.log(result);
             })
-        );
-    }, [nodeName, setNodes]);
+        }
 
-    useEffect(() => {
-        setNodes((nds) =>
-            nds.map((node) => {
-                if (selectedNode != null && node.id === selectedNode.id) {
-                    node.style = { ...node.style, backgroundColor: nodeBg };
-                }
-
-                return node;
-            })
-        );
-    }, [nodeBg, setNodes]);
-
-    const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
-
-    const onDragOver = useCallback((event) => {
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'move';
-    }, []);
-
-    const onDrop = useCallback(
-        (event) => {
-            event.preventDefault();
-
-            const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
-            const type = event.dataTransfer.getData('application/reactflow');
-
-            if (typeof type === 'undefined' || !type) {
-                return;
+        function onNodesSelected(node) {
+            if (node) {
+                setNodeName(node.data.label);
+                setNodeBg(node.style.backgroundColor);
+                setSelectNode(node);
             }
-
-            const position = reactFlowInstance.project({
-                x: event.clientX - reactFlowBounds.left,
-                y: event.clientY - reactFlowBounds.top,
-            });
-            const newNode = {
-                id: getId(),
-                type,
-                position,
-                data: { label: `${type} node` },
-                style: { backgroundColor: nodeBg },
-            };
-
-            setNodes((nds) => nds.concat(newNode));
-
-        },
-        [reactFlowInstance]
-    );
-
-    return (
-        <div className="dndflow" style={{
-            "display": "flex",
-            "flexDirection": "row",
-            paddingLeft: "2rem",
-            paddingRight: "2rem"
-        }}>
-            <div style={{ "flex": 3 }}>
-                <ReactFlowProvider>
-                    <div style={{ height: "80vh" }} ref={reactFlowWrapper}>
-                        <ReactFlow
-                            nodes={nodes}
-                            edges={edges}
-                            onNodesChange={onNodesChange}
-                            onEdgesChange={onEdgesChange}
-                            onConnect={onConnect}
-                            onSelectionChange={({ nodes }) => onNodesSelected(nodes[0])}
-                            onInit={setReactFlowInstance}
-                            onDrop={onDrop}
-                            onDragOver={onDragOver}
-                            fitView
-                        >
-                            <Controls />
-                        </ReactFlow>
-                    </div>
-                </ReactFlowProvider>
-            </div>
-            <div style={{ flex: 1, borderLeft: "1px solid #ccc" }}>
-                <Sidebar nodes={nodes} edges={edges} graphName={graphName} />
-                <form class="ui form updatenode_controls" style={{ display: selectedNode ? "block" : "none" }}>
-                    <h2>Customize the selected node</h2>
-
-                    <div class="field">
-                        <label>Label</label>
-                        <input value={nodeName} onChange={(evt) => changeNodeName(evt.target.value)} />
-                    </div>
-                    <div class="field">
-                        <label className="updatenode_bglabel">Background</label>
-                        <input value={nodeBg} onChange={(evt) => changeBgColor(evt.target.value)} />
-                    </div>
-                </form>
-                <form class="ui form updatenode_controls" style={{ display: nodes ? "block" : "none" }}>
-                    <h2>Save your graph to a file</h2>
-                    <div class="field">
-                        <label>Name</label>
-                        <input placeholder='your_application' value={graphName} onChange={(evt) => setGraphName(evt.target.value)} />
-                    </div>
-                    <Button color='green' onClick={() => saveFile()}>Save</Button>
-                </form>
-            </div>
-        </div>
-    );
-};
-
-
-export default (props) => (
-    <OneOSService.Consumer>
-        {
-            (sys) => (<ApplicationBuilder {...props} sys={sys} />)
         }
-    </OneOSService.Consumer>
-)
+
+        function changeNodeName(name) {
+            if (selectedNode) {
+                setNodeName(name);
+            }
+        }
+
+        function changeBgColor(color) {
+            if (selectedNode) {
+                setNodeBg(color);
+            }
+        }
+
+        useEffect(() => {
+            setNodes((nds) =>
+                nds.map((node) => {
+                    if (selectedNode != null && node.id === selectedNode.id) {
+                        node.data = {
+                            ...node.data,
+                            label: nodeName,
+                        };
+                    }
+
+                    return node;
+                })
+            );
+        }, [nodeName, setNodes]);
+
+        useEffect(() => {
+            setNodes((nds) =>
+                nds.map((node) => {
+                    if (selectedNode != null && node.id === selectedNode.id) {
+                        node.style = { ...node.style, backgroundColor: nodeBg };
+                    }
+
+                    return node;
+                })
+            );
+        }, [nodeBg, setNodes]);
+
+        const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
+        const onDragOver = useCallback((event) => {
+            event.preventDefault();
+            event.dataTransfer.dropEffect = 'move';
+        }, []);
+
+        const onDrop = useCallback(
+            (event) => {
+                event.preventDefault();
+
+                const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+                const type = event.dataTransfer.getData('application/reactflow');
+
+                if (typeof type === 'undefined' || !type) {
+                    return;
+                }
+
+                const position = reactFlowInstance.project({
+                    x: event.clientX - reactFlowBounds.left,
+                    y: event.clientY - reactFlowBounds.top,
+                });
+                const newNode = {
+                    id: getId(),
+                    type,
+                    position,
+                    data: { label: `${type} node` },
+                    style: { backgroundColor: nodeBg },
+                };
+
+                setNodes((nds) => nds.concat(newNode));
+
+            },
+            [reactFlowInstance]
+        );
+
+        return (
+            <div className="dndflow" style={{
+                "display": "flex",
+                "flexDirection": "row",
+                paddingLeft: "2rem",
+                paddingRight: "2rem"
+            }}>
+                <div style={{ "flex": 3 }}>
+                    <ReactFlowProvider>
+                        <div style={{ height: "80vh" }} ref={reactFlowWrapper}>
+                            <ReactFlow
+                                nodes={nodes}
+                                edges={edges}
+                                onNodesChange={onNodesChange}
+                                onEdgesChange={onEdgesChange}
+                                onConnect={onConnect}
+                                onSelectionChange={({ nodes }) => onNodesSelected(nodes[0])}
+                                onInit={setReactFlowInstance}
+                                onDrop={onDrop}
+                                onDragOver={onDragOver}
+                                fitView
+                            >
+                                <Controls />
+                            </ReactFlow>
+                        </div>
+                    </ReactFlowProvider>
+                </div>
+                <div style={{ flex: 1, borderLeft: "1px solid #ccc" }}>
+                    <Sidebar nodes={nodes} edges={edges} graphName={graphName} />
+                    <form class="ui form updatenode_controls" style={{ display: selectedNode ? "block" : "none" }}>
+                        <h2>Customize the selected node</h2>
+
+                        <div class="field">
+                            <label>Label</label>
+                            <input value={nodeName} onChange={(evt) => changeNodeName(evt.target.value)} />
+                        </div>
+                        <div class="field">
+                            <label className="updatenode_bglabel">Background</label>
+                            <input value={nodeBg} onChange={(evt) => changeBgColor(evt.target.value)} />
+                        </div>
+                    </form>
+                    <form class="ui form updatenode_controls" style={{ display: nodes ? "block" : "none" }}>
+                        <h2>Save your graph to a file</h2>
+                        <div class="field">
+                            <label>Name</label>
+                            <input placeholder='your_application' value={graphName} onChange={(evt) => setGraphName(evt.target.value)} />
+                        </div>
+                        <Button color='green' onClick={() => saveFile()}>Save</Button>
+                    </form>
+                </div>
+            </div>
+        );
+    };
+
+
+    export default (props) => (
+        <OneOSService.Consumer>
+            {
+                (sys) => (<ApplicationBuilder {...props} sys={sys} />)
+            }
+        </OneOSService.Consumer>
+    )
